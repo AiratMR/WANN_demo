5322af780223bbddff3580b61f141510ecc16bb3
Айрат Мухаметгараев <airat9753@gmail.com>
2020-03-11 21:48:15 +0300

    Добавил функцию добавления новой связи

--- ModelGenerator/WANNModelGenerator.py
+++ ModelGenerator/WANNModelGenerator.py
@@ -4,7 +4,7 @@ import random
 
 from typing import List
 from Model import WANNModel
-from Model.Nodes import BaseNode
+from Model.Connections import Connection
 from Utils import get_random_function
 
 EVAL_WEIGHTS = [-2.0, -1.0, 0.1, 1.0, 2.0]
@@ -17,8 +17,8 @@ def generate_wann_model(x_train, y_train, tol: float = 0.1, niter: int = 50, gen
     result = _sort_models_by_error(x_scaled, y_scaled, generation)
 
     winner_model = next(model for model in generation if model.model_id == result[0][1])
-    nodes = winner_model.get_all_nodes()
-    _change_activation_function(nodes)
+    if _add_connection(winner_model):
+        print('success')
 
     return generation
 
@@ -44,6 +44,28 @@ def _sort_models_by_error(x_train, y_train, generation):
     return sorted((value, key) for (key, value) in model_result.items())
 
 
-def _change_activation_function(nodes: List[BaseNode]):
+def _change_activation_function(model):
+    nodes = model.get_all_nodes()
     node = nodes[random.randint(0, len(nodes) - 1)]
     node.activation = get_random_function()
+
+
+def _add_connection(model) -> bool:
+    all_nodes = model.get_all_nodes()
+    node = all_nodes[random.randint(0, len(all_nodes) - 1)]
+
+    candidate_layers = []
+    for layer in model.layers:
+        if layer.layer_id == node.layer_id:
+            break
+        candidate_layers.append(layer)
+
+    candidate_nodes = [node for layer in candidate_layers for node in layer.nodes]
+    candidate_node = candidate_nodes[random.randint(0, len(candidate_nodes) - 1)]
+
+    connection = Connection(candidate_node, model.weight)
+    if node.prev_connections.is_valid_connection(connection):
+        node.prev_connections.add_connection(connection)
+        return True
+
+    return False
